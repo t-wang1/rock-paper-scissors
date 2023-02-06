dd5f1bf080097f446f0235108285fb61bc7ed921
Tiffany <twk1518@gmail.com>
2023-02-06 13:35:34 -0500

    Create three buttons, one for each player selection

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let items = ["rock", "paper", "scissors"];
     let choice = items[Math.floor(Math.random() * items.length)];
-    console.log(choice);
     return choice;
 }
 
@@ -10,21 +12,41 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie!";
     }
     else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "paper") || (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") || (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")) {
-        return "You Lose. " + computerSelection + " beats " + playerSelection.toLowerCase();
+        computerScore++;
+        return " You Lose. " + computerSelection + " beats " + playerSelection.toLowerCase() + ". Your opponent's current record is " + computerScore;
     }
     else {
-        return "You Win! " + playerSelection.toLowerCase() + " beats " + computerSelection;
+        playerScore++;
+        return " You Win! " + playerSelection.toLowerCase() + " beats " + computerSelection + ". Your current record is " + playerScore;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Enter rock, paper, or scissors: ");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+function countScore() {
+    if (playerScore > computerScore) {
+        return "You won the game! ";
+    }
+    else if (playerScore === computerScore) {
+        return "You tied ";
+    }
+    else {
+        return "You lost the game ";
     }
 }
 
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () =>  {
+    alert("you chose rock");
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () =>  {
+    alert("you chose paper");
+});
+
+const scissors = document.querySelector('#scissors');
+rock.addEventListener('click', () =>  {
+    alert("you chose scissors");
+});
+
+countScore();
 game();
-
-
